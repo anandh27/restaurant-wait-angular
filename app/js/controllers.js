52abfcdf68e351224d8873acfbb54fc72a418d8d
Tom Brennan <tommybrennan@gmail.com>
2014-09-24 10:37:43 -0400

    added the sendTextMessage scope to add text messages to firebase

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,15 +7,25 @@ angular.module('myApp.controllers', [])
 
   }])
   .controller('WaitlistController', ['$scope', '$firebase', function($scope, $firebase) {
-    var partiesRef = new Firebase('https://waitandeat-tomb.firebaseio.com/');
+    // Connect scope.parties to live firebase data
+    var partiesRef = new Firebase('https://waitandeat-tomb.firebaseio.com/parties');
 
     $scope.parties = $firebase(partiesRef);
 
+    // Object to store data from the waitlist form
     $scope.newParty = {name: '', phone: '', size: ''};
 
+    // Function to save a new party to the waitlist
     $scope.saveParty = function() {
       $scope.parties.$add($scope.newParty);
       $scope.newParty = {name: '', phone: '', size: ''};
+    };
+
+    //Function to send text message to a party.
+    $scope.sendTextMessage = function(phoneNumber) {
+      var textMessageRef = new Firebase('https://waitandeat-tomb.firebaseio.com/textMessages');
+      var textMessages = $firebase(textMessageRef);
+      textMessages.$add({phoneNumber: phoneNumber});
     };
   }]);
 
